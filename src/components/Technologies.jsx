413d65bb1e65ea90381e528c703873b1a21a4294
Fayazuddin Shaik <shaikfayazuddin18@gmail.com>
2025-10-16 17:49:30 +0530

    feat: adding famer motions

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -7,44 +7,106 @@ import { SiMongodb } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaNodeJs } from "react-icons/fa";
 import { FaDocker } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [-10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 const Technologies = () => {
   return (
     <div className="pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="p-4">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+          className="p-4"
+        >
           <IoLogoJavascript className="text-7xl text-yellow-300" />
-        </div>
+        </motion.div>
 
-        <div className="p-4">
+        <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        
-        <div className="p-4">
+        </motion.div>
+
+        <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+          className="p-4"
+        >
           <TbBrandNextjs className="text-7xl" />
-        </div>
+        </motion.div>
 
-        <div className="p-4">
+        <motion.div initial="intial"
+        animate="animate"
+        variants={iconVariants(3)} className="p-4">
           <FaNodeJs className="text-7xl text-green-500" />
-        </div>
+        </motion.div>
 
-        <div className="p-4">
+        <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+          className="p-4"
+        >
           <SiMongodb className="text-7xl text-green-500" />
-        </div>
-        {/* <div className="p-4">
-          <DiRedis className="text-7xl text-red-700" />
-        </div> */}
-        
-        <div className="p-4">
-          <BiLogoPostgresql className="text-7xl text-sky-700"/>
-        </div>
+        </motion.div>
 
-        <div className="p-4">
-          <FaDocker className="text-7xl text-blue-500"/>
-        </div>
-        
-      </div>
+        {/* <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
+          <DiRedis className="text-7xl text-red-700" />
+        </motion.div> */}
+
+        <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+        </motion.div>
+
+        <motion.div
+          initial="intial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+          className="p-4"
+        >
+          <FaDocker className="text-7xl text-blue-500" />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
